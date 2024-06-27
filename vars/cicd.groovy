adcc31591e1226a68c6b8167988316f6c26c5946
Imrantech3057 <149435373+Imrantech3057@users.noreply.github.com>
2024-06-28 02:50:10 +0300

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,6 +8,6 @@ def mavenBuild()
 }
 def deploy(jobname,ip,context)
 {
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
 
