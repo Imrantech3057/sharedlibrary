e581e6a769824a482b4a2ffa7bd783d58be93276
Imrantech3057 <149435373+Imrantech3057@users.noreply.github.com>
2024-06-28 02:05:22 +0300

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,15 +2,4 @@ def gitDownload(repo)
 {
   git "https://github.com/Imrantech3057/${repo}".git
 }
-def mavenBuild()
-{
-  sh 'mvn package'
-}
-def deploy(jobname,ip,context)
-{
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
-}
-def runselenium(jobname)
-{
-  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar
-}
+
